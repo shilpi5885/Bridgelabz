19a816085a60a5aeff696fcb41dd240bc9221bc1
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2021-11-23 13:19:23 +0530

    Create numberToWeekDayCase.sh

---
+++ day 5/numberToWeekDayCase.sh
@@ -0,0 +1,29 @@
+echo "Enter a number"
+read num
+	
+case $num in
+   1)
+      echo "Monday"
+      ;;
+   2)
+      echo "Tuesday"
+      ;;
+   3)
+      echo "Wednesday"
+      ;;
+   4)
+      echo "Thursday"
+      ;;
+   5)
+      echo "Friday"
+      ;;
+   6)
+      echo "Saturday"
+      ;;
+   7)
+      echo "Sunday"
+      ;;
+   *)
+     echo "Invalid Input ..."
+     ;;
+esac
