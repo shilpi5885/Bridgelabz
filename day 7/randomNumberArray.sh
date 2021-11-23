a40b44bc22391238839d5e9d4a53988f9cc024ec
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2021-11-23 14:48:39 +0530

    Create randomNumberArray.sh

---
+++ day 7/randomNumberArray.sh
@@ -0,0 +1,26 @@
+#Extend the above program to sort the array and then find the 2nd largest and the 2nd smallest element.
+
+for ((i=0; i <10; i++))
+do	
+	array[$i]=$((RANDOM%1000))
+	echo ${array[i]}
+done
+
+for ((i = 0; i<10; i++))
+do
+    
+    for((j = 0; j<10-i-1; j++))
+    do
+    
+        if [ ${array[j]} -gt ${array[$((j+1))]} ]
+        then
+            temp=${array[j]}
+            array[$j]=${array[$((j+1))]}  
+            array[$((j+1))]=$temp
+        fi
+    done
+done
+echo ${array[@]}
+
+echo "Second Largest = ${array[8]}"
+echo "Second Smallest = ${array[1]}"
