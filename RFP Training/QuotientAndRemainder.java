04ee7d067a26d2a82d262f70869890f81c6f12a9
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-01-14 20:54:28 +0530

    Add files via upload

---
+++ RFP Training/QuotientAndRemainder.java
@@ -0,0 +1,17 @@
+/*Java Program to Compute Quotient and Remainder*/
+
+package com.bridgelabz.demo;
+
+public class QuotientAndRemainder {
+    public static void main(String[] args)
+    {
+    	int dividend = Integer.parseInt(args[0]);
+    	int divisor = Integer.parseInt(args[1]);
+    	
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
