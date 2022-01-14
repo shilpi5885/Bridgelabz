3e3cbe97a9b1e6cb2018322da127ea3dd9601180
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-01-14 12:33:14 +0530

    Add files via upload

---
+++ RFP Training/Point.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.demo;
+
+class Point {
+    double x, y;
+    Point (double xcoord, double ycoord) {
+        this.x = xcoord;
+        this.y = ycoord;
+    }
+    public double getX() { return x; }
+    public double getY() { return y; }
+}
