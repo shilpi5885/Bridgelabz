11c39dcecec402b48e31ae950985a54163f01f19
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-01-14 19:47:57 +0530

    Add files via upload

---
+++ RFP Training/HarmonicNumber.java
@@ -0,0 +1,20 @@
+/*Harmonic Number*/
+
+package com.bridgelabz.demo;
+import java.util.Scanner;
+class HarmonicNumber
+{
+public static void main(String[] args)
+    {
+        System.out.print("Enter any number : ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        double result = 0;
+        for(;num>0;num--) {
+        	System.out.println("1/"+num);
+            result = result + (double)1 / num;
+        }
+            System.out.print(result + ", ");
+            s.close();
+    }
+}
---
+++ RFP Training/PowerOfTwo.java
@@ -0,0 +1,21 @@
+/*Java program takes a command-line argument N and prints a table of the powers of 2 that are less than or equal to 2^N.*/
+
+package com.bridgelabz.demo;
+
+public class PowerOfTwo {
+
+public static void main(String[] args) {
+System.out.println("Enter a Number: ");
+int n = Integer.parseInt(args[0]);
+int powerOfTwo = 1;
+
+for(int i = 0; i <= n; i++) {
+System.out.println("2 to the power "+i+ " is " + powerOfTwo); 
+
+powerOfTwo = 2 * powerOfTwo;
+
+}
+
+}
+
+}
