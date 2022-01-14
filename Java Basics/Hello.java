99a2f85568a3ce25f393c9918ccf0f0dd571f5b1
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-01-14 12:31:36 +0530

    Delete Java Basics directory

--- Java Basics/Hello.java
+++
@@ -1,9 +0,0 @@
-/*Java program to display any message*/
-
-public class Hello {
-
-	public static void main(String[] args) {
-				System.out.println("Hello World");
-			}
-
-}
--- Java Basics/Name.java
+++
@@ -1,8 +0,0 @@
-/*Java program to get the name using the command line*/
-
-public class Name {
-	public static void main(String[] args) {
-		System.out.println("Name: "+args[0]);
-	}
-
-}
