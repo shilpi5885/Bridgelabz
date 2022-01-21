420e947af5775bb3b1c1424e3145cf29ad0f8ea6
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-01-21 14:19:12 +0530

    Add files via upload

---
+++ day6/ReverseOfNumber.java
@@ -0,0 +1,22 @@
+//Reverse of a number
+
+package com.bridgelabz.demo;
+
+import java.util.Scanner;
+
+public class ReverseOfNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number: ");
+		Scanner sc = new Scanner(System.in);
+		long number = sc.nextLong();
+		long reverse = 0;
+		while (number != 0) {
+			long remainder = number % 10;
+			reverse = reverse * 10 + remainder;
+			number = number / 10;
+		}
+		System.out.println("The reverse of the given number is: " + reverse);
+		sc.close();
+	}
+}
