c969d862d95c1d49a119726ccce2ad3760af09c9
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-01-21 14:11:19 +0530

    Add files via upload

---
+++ RFP Training/day5/VowelOrConsonant.java
@@ -0,0 +1,33 @@
+/*Java Program to Check Whether an Alphabet is Vowel or Consonant*/
+
+package com.bridgelabz.demo;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+		System.out.print("Enter an albhabet : ");
+		Scanner s = new Scanner(System.in);
+		char letter = s.next().charAt(0);
+		switch (letter) {
+
+		case 'a':
+		case 'A':
+		case 'e':
+		case 'E':
+		case 'i':
+		case 'I':
+		case 'o':
+		case 'O':
+		case 'u':
+		case 'U':
+			System.out.println("Given alphabet is a VOWEL");
+			break;
+		default:
+			System.out.println("Given alphabet is a CONSONANT");
+		}
+		s.close();
+	}
+
+}
