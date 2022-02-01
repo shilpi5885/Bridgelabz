137d0c209b8cf9b07cf64b0881c5c7c20b660167
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-02-01 14:18:50 +0530

    Update EmployeeWageComputation.java

--- RFP Training/day10/EmployeeWageComputation.java
+++ RFP Training/day10/EmployeeWageComputation.java
@@ -1,35 +1,42 @@
 /*Employee Wage Computation*/
 
-package com.bridgelabz.demo;
 
 import java.lang.Math;
 
 public class EmployeeWageComputation {
 
-	public static int calculateEmpWage(int wagePerHour, int workingdays, int maxHours) {
-		int workHours = 0, totalWorkHours = 0, workingdaysPerMonth =0; 
+	public static int calculateEmpWage() { 
 		System.out.println("Employee Wage Computaion Program On Master Branch");
-		while (totalWorkHours <= maxHours && workingdaysPerMonth < workingdays) {
+		
+		int totalWorkHours =0, workingdaysPerMonth = 0, workHours = 0;
+		
+		EmpWageBuilder company = new EmpWageBuilder("XYZ",120,25,26);
+		
+		while (totalWorkHours <= company.totalWorkHours && workingdaysPerMonth < company.workingdaysPerMonth) {
 			workingdaysPerMonth++;
 			int empCheck = (int) (Math.random()*10 % 2);
 			switch (empCheck)
 			{
 			case 0:
-				workHours=8; //Employee is present full time
+				workHours=8;
+				//System.out.println("Employee is present full time");
 				break;
 			case 1:
-				workHours=4;//Employee is present half time
+				workHours=4;
+				//System.out.println("Employee is present full time");
 				break;
 			default:
-				workHours=0;//Employee is absent
+				workHours=0;
+				//System.out.println("Employee is absent");
 				break;
 			}
 			totalWorkHours += workHours;
 		}
-		int empWage = totalWorkHours * wagePerHour;
+		int empWage = totalWorkHours * company.wagePerHour;
 		return empWage;
 	}
+
 	public static void main(String[] args) {
-		System.out.println("Employee Wage = "+calculateEmpWage(20,20,100));
+		System.out.println("Employee Wage = " + calculateEmpWage());		
 	}
 }
