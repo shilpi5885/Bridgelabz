6ac91aeef9e95a6edc3af706c8dbe48de6b4ede2
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-02-01 19:59:30 +0530

    Add files via upload

---
+++ RFP Training/day10/CompanyEmpWage.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.demo;
+
+public class CompanyEmpWage {
+	
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
+	public int totalEmpWage;
+	
+	public CompanyEmpWage(String company, int empRatePerHour, 
+			int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+		this.totalEmpWage = 0;
+	}
+	
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "CompanyEmpWage [totalEmpWage=" + totalEmpWage + "]";
+	}
+}
--- RFP Training/day10/EmpWageBuilder.java
+++ RFP Training/day10/EmpWageBuilder.java
@@ -1,15 +1,72 @@
 package com.bridgelabz.demo;
 
-public class EmpWageBuilder {
-	String companyName;
-	int wagePerHour;
-	int workingdaysPerMonth;
-	int totalWorkHours;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
-	EmpWageBuilder(String companyName, int wagePerHour, int workingdaysPerMonth, int totalWorkHours ){
-		this.companyName = companyName;
-		this.wagePerHour = wagePerHour;
-		this.workingdaysPerMonth = workingdaysPerMonth;
-		this.totalWorkHours = totalWorkHours;
+public class EmpWageBuilder implements IComputeEmpWage {
+	private int numOfCompany = 0;
+	private LinkedList<CompanyEmpWage> companyEmpWageList;
+	private Map<String,CompanyEmpWage> companyToEmpWageMap;
+	
+	public EmpWageBuilder() {
+		companyEmpWageList = new LinkedList<>();
+		companyToEmpWageMap = new HashMap<>();
+	}
+	
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		CompanyEmpWage companyEmpWage = new  CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		companyEmpWageList.add(companyEmpWage);
+		companyToEmpWageMap.put(company, companyEmpWage);
+	}
+	
+	public void computeEmpWage() {
+		for(int i = 0; i < companyEmpWageList.size(); i++) {
+			CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);	
+		}	
+	}
+	
+	@Override
+	public int getTotalWage(String company) {
+		return companyToEmpWageMap.get(company).totalEmpWage;
+	}
+	
+	public int computeEmpWage(CompanyEmpWage companyEmpWage) {
+		System.out.println("Employee Wage Computaion Program On Master Branch");
+
+		int totalWorkHours = 0, workingdaysPerMonth = 0, workHours = 0;
+
+		while (totalWorkHours <= companyEmpWage.maxHoursPerMonth
+				&& workingdaysPerMonth < companyEmpWage.numOfWorkingDays) {
+			workingdaysPerMonth++;
+			int empCheck = (int) (Math.random() * 10 % 2);
+			switch (empCheck) {
+			case 0:
+				workHours = 8;
+				// System.out.println("Employee is present full time");
+				break;
+			case 1:
+				workHours = 4;
+				// System.out.println("Employee is present full time");
+				break;
+			default:
+				workHours = 0;
+				// System.out.println("Employee is absent");
+				break;
+			}
+			totalWorkHours += workHours;
+		}
+		int empWage = totalWorkHours * companyEmpWage.empRatePerHour;
+		return empWage;
+	}
+	
+	public static void main(String[] args) {
+		IComputeEmpWage empWageBuilder = new EmpWageBuilder();
+		empWageBuilder.addCompanyEmpWage("Amazon", 30, 24, 80);
+		empWageBuilder.computeEmpWage();
+		System.out.println("Total wage for Amazon Company "+empWageBuilder.getTotalWage("Amazon"));
 	}
 }
+	
