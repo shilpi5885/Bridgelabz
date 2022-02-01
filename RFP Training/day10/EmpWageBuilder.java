e46473ab62696ec61ccdd64dd540b42279207bb0
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-02-01 14:21:38 +0530

    Update EmpWageBuilder.java

--- RFP Training/day10/EmpWageBuilder.java
+++ RFP Training/day10/EmpWageBuilder.java
@@ -5,30 +5,11 @@ public class EmpWageBuilder {
 	int wagePerHour;
 	int workingdaysPerMonth;
 	int totalWorkHours;
-	public String getCompanyName() {
-		return companyName;
-	}
-	public void setCompanyName(String companyName) {
+
+	EmpWageBuilder(String companyName, int wagePerHour, int workingdaysPerMonth, int totalWorkHours ){
 		this.companyName = companyName;
-	}
-	public int getWagePerHour() {
-		return wagePerHour;
-	}
-	public void setWagePerHour(int wagePerHour) {
 		this.wagePerHour = wagePerHour;
-	}
-	public int getWorkingdaysPerMonth() {
-		return workingdaysPerMonth;
-	}
-	public void setWorkingdaysPerMonth(int workingdaysPerMonth) {
 		this.workingdaysPerMonth = workingdaysPerMonth;
-	}
-	public int getTotalWorkHours() {
-		return totalWorkHours;
-	}
-	public void setTotalWorkHours(int totalWorkHours) {
 		this.totalWorkHours = totalWorkHours;
 	}
-	
-	
 }
